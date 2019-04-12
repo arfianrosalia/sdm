9e2dc29e57bc82430db51ef157a1fa577328e2d4
arfianrosalia <arfian.rosalia@gmail.com>
2019-04-12 09:54:37 +0700

    NEW TEMPLATE

---
+++ application/views/view-sidebar.php
@@ -0,0 +1,51 @@
+<li class="active">
+    <a href="index.html">
+        <i class="material-icons">insert_chart</i>
+        <span>Dashboard</span>
+    </a>
+</li>
+<li>
+    <a href="index.html">
+        <i class="material-icons">people</i>
+        <span>Pegawai</span>
+    </a>
+</li>
+<li>
+    <a href="javascript:void(0);" class="menu-toggle">
+        <i class="material-icons">storage</i>
+        <span>Master</span>
+    </a>
+    <ul class="ml-menu">
+        <li>
+            <a href="pages/examples/profile.html">Department</a>
+        </li>
+        <li>
+            <a href="pages/examples/sign-in.html">Jabatan</a>
+        </li>
+        <li>
+            <a href="pages/examples/sign-up.html">Status Karyawan</a>
+        </li>
+        <li>
+            <a href="pages/examples/forgot-password.html">Gelar Nama</a>
+        </li>
+        <li>
+            <a href="pages/examples/blank.html">Pendidikan</a>
+        </li>
+        <li>
+            <a href="javascript:void(0);" class="menu-toggle">
+                Wilayah
+            </a>
+            <ul class="ml-menu">
+                <li>
+                    <a href="pages/examples/profile.html">Provinsi</a>
+                </li>
+                <li>
+                    <a href="pages/examples/sign-in.html">Kota</a>
+                </li>
+                <li>
+                    <a href="pages/examples/sign-up.html">Wilayah</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</li>
