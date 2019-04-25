df87c99cb4f607e1827c9295269f7ca664b892e5
arfianrosalia <arfian.rosalia@gmail.com>
2019-04-25 16:40:43 +0700

    Rev Form 1

--- application/views/view-pegawai.php
+++ application/views/view-pegawai.php
@@ -162,6 +162,45 @@
         </div> -->
     </div>
 
+
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-9" id="form_detail">
+        <div class="row clearfix">
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="info-box bg-pink hover-expand-effect">
+                    <div class="icon">
+                        <i class="material-icons">playlist_add_check</i>
+                    </div>
+                    <div class="content">
+                        <div class="text">ABSENSI</div>
+                        <div class="number count-to" data-from="0" data-to="125" data-speed="15" data-fresh-interval="20"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="info-box bg-light-green hover-expand-effect">
+                    <div class="icon">
+                        <i class="material-icons">forum</i>
+                    </div>
+                    <div class="content">
+                        <div class="text">BLABLA</div>
+                        <div class="number count-to" data-from="0" data-to="243" data-speed="1000" data-fresh-interval="20"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="info-box bg-orange hover-expand-effect">
+                    <div class="icon">
+                        <i class="material-icons">person_add</i>
+                    </div>
+                    <div class="content">
+                        <div class="text">BLABLA</div>
+                        <div class="number count-to" data-from="0" data-to="1225" data-speed="1000" data-fresh-interval="20"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-9" id="form_detail">
         <div class="card" id="panel-foto" style="display: none;">
             <div class="body">
